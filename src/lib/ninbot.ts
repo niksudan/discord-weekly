5f7a67942a3b83cd2de9f7bd5084d58859c5acdf
Nik Sudan <niksudan@gmail.com>
2019-07-13 23:03:08 +0100

    Fix message aggregation and successfully update a specified playlist

--- src/lib/ninbot.ts
+++ src/lib/ninbot.ts
@@ -42,48 +42,46 @@ export default class Ninbot {
    */
   public async fetchMessages(
     channel: TextChannel,
-    targetDate: moment.Moment,
-    currentMessages: Messages = new Collection<string, Message>(),
-    beforeDate?: moment.Moment
+    fromDate: moment.Moment,
+    toDate: moment.Moment,
+    messages: Messages = new Collection<string, Message>()
   ): Promise<Messages> {
-    console.log("Fetching messages...");
-    let options = {};
-    if (beforeDate) {
-      options = {
-        before: SnowflakeUtil.generate(
-          beforeDate.subtract(1, "second").toDate()
-        )
-      };
+    if (fromDate.isAfter(toDate)) {
+      return messages;
     }
-    const newMessages = await channel.fetchMessages(options);
+
+    // Fetch 50 messages before the specified end date
+    console.log(
+      `Fetching messages from ${fromDate.toString()} to ${toDate.toString()}...`
+    );
+    const newMessages = await channel.fetchMessages({
+      before: SnowflakeUtil.generate(toDate.toDate())
+    });
 
     // If the payload is empty, there are no more messages left in the channel
     if (newMessages.size === 0) {
-      console.log("No more messages to fetch in channel");
-      return currentMessages;
+      return messages;
     }
 
     // We're only interested in getting the messages before the target date
     const messagesToAdd = newMessages.filter(message =>
-      moment(message.createdAt).isSameOrAfter(targetDate)
+      moment(message.createdAt).isBetween(fromDate, toDate)
     );
-    const messages = currentMessages.concat(messagesToAdd);
 
     // If all messages were after the target date, fetch for more
-    if (messagesToAdd.size === 50) {
-      const lastMessageDate = moment(messagesToAdd.last().createdAt);
-      return this.fetchMessages(channel, targetDate, messages, lastMessageDate);
-    }
-
-    console.log(`No more messages to fetch after ${targetDate.toString()}`);
-    return messages;
+    return this.fetchMessages(
+      channel,
+      fromDate,
+      moment(newMessages.last().createdAt).subtract(1, "ms"),
+      messages.concat(messagesToAdd)
+    );
   }
 
   /**
    * Generate a Spotify playlist
    */
-  public async generatePlaylist(spotify: Spotify) {
-    console.log("Generating playlist...");
+  public async generatePlaylist(spotify: Spotify, weeksAgo = 1) {
+    console.log(`Generating playlist from ${weeksAgo} week(s) ago...`);
     const channel = this.guild.channels.find(
       channel => channel.name === "non-nin-music" && channel.type === "text"
     ) as TextChannel;
@@ -91,30 +89,60 @@ export default class Ninbot {
       return;
     }
 
+    // Calculate the date range
+    const fromDate = moment()
+      .startOf("isoWeek")
+      .startOf("day")
+      .subtract(weeksAgo, "week");
+    const toDate = fromDate.clone().endOf("isoWeek");
+
+    // Reset playlist
+    await spotify.clearPlaylist();
+    await spotify.renamePlaylist(
+      `${process.env.PLAYLIST_NAME} (${fromDate.format(
+        "Do MMMM"
+      )} - ${toDate.format("Do MMMM")})`
+    );
+
     // Fetch all messages from the channel within the past week
-    const targetDate = moment()
-      .subtract(1, "week")
-      .startOf("day");
-    const messages = await this.fetchMessages(channel, targetDate);
-    console.log(`${messages.size} message(s) in total were fetched`);
-    if (messages.size === 0) {
-      return;
-    }
+    const messages = await this.fetchMessages(channel, fromDate, toDate);
+    console.log(
+      `${messages.size} message(s) were fetched in total`,
+      messages.map(message => message.content)
+    );
 
     // Filter the messages to those that contain Spotify links
-    let spotifyUrls = [];
+    let spotifyUrls: string[] = [];
     messages.forEach(message => {
       const match = message.content.match(
-        /(https:\/\/open.spotify.com\/track\/[^\s]+)/g
+        /https:\/\/open.spotify.com\/track\/([^? ]+)/gi
       );
-      if (match && match.length > 0) {
-        spotifyUrls = spotifyUrls.concat(match);
+      if (match) {
+        spotifyUrls = spotifyUrls.concat(
+          match.map(
+            url =>
+              `spotify:track:${url.replace(
+                /https:\/\/open.spotify.com\/track\//gi,
+                ""
+              )}`
+          )
+        );
       }
     });
 
     console.log(
       `${spotifyUrls.length} Spotify track(s) were detected`,
       spotifyUrls
+    );
+
+    // Update the playlist with the tracks
+    if (spotifyUrls.length) {
+      await spotify.addTracksToPlaylist(spotifyUrls.reverse());
+    }
+
+    console.log(
+      "Playlist was updated successfully",
+      `https://open.spotify.com/playlist/${process.env.PLAYLIST_ID}`
     );
   }
 }
