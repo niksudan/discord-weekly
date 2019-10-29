8d4d537577a1d1a036d9ef754d31ebccdad1ed10
Nik Sudan <niksudan@gmail.com>
2019-10-29 20:12:07 +0000

    Don't crash if not logged into Spotify

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import Ninbot from "./lib/ninbot";
-import Server from "./lib/server";
-import Spotify from "./lib/spotify";
-import * as Sentry from "@sentry/node";
+import Ninbot from './lib/ninbot';
+import Server from './lib/server';
+import Spotify from './lib/spotify';
+import * as Sentry from '@sentry/node';
 
-require("dotenv").config();
+require('dotenv').config();
 
 if (process.env.SENTRY_DSN) {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
@@ -11,18 +11,20 @@ if (process.env.SENTRY_DSN) {
 
 (async () => {
   setTimeout(() => {
-    console.log("Took too long, exiting");
+    console.log('Took too long, exiting');
     process.exit(1);
   }, 1000 * 30);
 
   try {
     const spotify = new Spotify();
     new Server(spotify);
+
     if (!spotify.isAuthenticated) {
       console.log("WARNING: Spotify features won't work until you log in");
-    } else {
-      await spotify.refreshTokens();
+      return;
     }
+    await spotify.refreshTokens();
+
     const ninbot = new Ninbot();
     await ninbot.login();
     await ninbot.generatePlaylist(spotify, 0);
